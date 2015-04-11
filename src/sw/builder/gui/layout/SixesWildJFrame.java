b0c424b471bd91dd7b6f3e18e61175fd7df3cf3d
scyevchak <scyevchak@8e881b8d-9e2d-430c-b0ad-9d7c9e80049a>
2015-04-11 19:18:14 +0000

    JFrame Basic class

---
+++ src/sw/builder/gui/layout/SixesWildJFrame.java
@@ -0,0 +1,17 @@
+package sw.builder.gui.layout;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+
+public class SixesWildJFrame extends JFrame {
+	public SixesWildJFrame() {
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 432, Short.MAX_VALUE)
+		);
+		getContentPane().setLayout(groupLayout);
+	}
+
+}
