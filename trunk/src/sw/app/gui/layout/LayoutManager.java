7699c7aba25b5a1e41d2ad43a73cd3327a9b9725
quangvu <quangvu@8e881b8d-9e2d-430c-b0ad-9d7c9e80049a>
2015-04-11 19:21:10 +0000

    GUI stuff

---
+++ trunk/src/sw/app/gui/layout/LayoutManager.java
@@ -0,0 +1,11 @@
+/**
+ * @file LayoutManager.java
+ * @date Apr 11, 2015 3:17:17 PM
+ * @author Tony Vu (quangvu@wpi.edu)
+ */
+package sw.app.gui.layout;
+
+/** Manages all the views used by Sixes Wild game application */
+public class LayoutManager {
+
+}
