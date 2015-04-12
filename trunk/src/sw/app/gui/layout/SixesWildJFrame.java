cc29f21aca6de33b6f3ef2cdf77bee6493d9da07
scyevchak <scyevchak@8e881b8d-9e2d-430c-b0ad-9d7c9e80049a>
2015-04-12 19:38:45 +0000

    JFrame with main method for testing to be removed

--- trunk/src/sw/app/gui/layout/SixesWildJFrame.java
+++ trunk/src/sw/app/gui/layout/SixesWildJFrame.java
@@ -1,34 +1,58 @@
 package sw.app.gui.layout;
 
 import java.awt.Dimension;
+import java.awt.SplashScreen;
 
 import javax.swing.JFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.SpringLayout;
 
-import sw.common.system.manager.LayoutManager;
+import org.omg.CORBA.INITIALIZE;
+import javax.swing.JPanel;
 
-public class SixesWildJFrame extends JFrame {
-	/** GENERATED DO NOT CHANGE */
-	private static final long serialVersionUID = -3162315874662880626L;
-
-	private LayoutManager layoutManager;
+public class SixesWildJFrame extends JFrame implements IView{
 	
-	Dimension size = new Dimension(800, 600);
+	public static void main(String[] args) {
+		SixesWildJFrame swj = new SixesWildJFrame();
+		
+		swj.setVisible(true);
+		swj.ssv.initialize();
+	}
+	
+	
+	SplashScreenView ssv = new SplashScreenView();
+	
 	
 	public SixesWildJFrame() {
-		setResizable(false);
-		setTitle("Sixes Wild");
-		setPreferredSize(size);
 		
+		JPanel panel = new JPanel();
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 432, Short.MAX_VALUE)
+				.addComponent(panel, GroupLayout.PREFERRED_SIZE, 1006, Short.MAX_VALUE)
 		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 724, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		panel.add(ssv);
+		setSize(1060, 850);
 		getContentPane().setLayout(groupLayout);
-		
-		this.layoutManager = new LayoutManager();
+
 	}
 
+	@Override
+	public void initialize() {
+		
+	}
+
+	@Override
+	public void cleanup() {
+		// TODO Auto-generated method stub
+		
+	}
 }
+
