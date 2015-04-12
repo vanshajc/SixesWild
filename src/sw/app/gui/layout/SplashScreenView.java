138794fc810d0a523cc400ed124ce1e04ce9ea15
scyevchak <scyevchak@8e881b8d-9e2d-430c-b0ad-9d7c9e80049a>
2015-04-12 19:16:25 +0000

    splash screen

---
+++ src/sw/app/gui/layout/SplashScreenView.java
@@ -0,0 +1,34 @@
+package sw.app.gui.layout;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class SplashScreenView extends JPanel implements IView{
+	public SplashScreenView() {
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(SplashScreenView.class.getResource("/sw/resource/image/SplashScreenResizedtest.png")));
+		add(lblNewLabel);
+	}
+	
+	public void initialize() {
+		
+		try {
+			Thread.sleep(5000);
+		} catch (Exception e) {
+		}
+		new MainMenuController(this);
+		
+	}
+
+	@Override
+	public void cleanup() {
+		// TODO Auto-generated method stub
+		
+	}
+}
