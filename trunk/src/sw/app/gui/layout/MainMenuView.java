116844ce16ef92a65bd0d7ae1dadba49e118d1b4
vchowdhary <vchowdhary@8e881b8d-9e2d-430c-b0ad-9d7c9e80049a>
2015-04-13 22:42:24 +0000

    Change main menu view

--- trunk/src/sw/app/gui/layout/MainMenuView.java
+++ trunk/src/sw/app/gui/layout/MainMenuView.java
@@ -1,5 +1,6 @@
 package sw.app.gui.layout;
 
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.ImageIcon;
@@ -20,20 +21,20 @@ public class MainMenuView extends JPanel implements IView{
 
 	@Override
 	public void initialize() {
-		
+		this.setBackground(Color.WHITE);
 		JButton btnPlay = new JButton("Play");
 		add(btnPlay);
-		btnPlay.setBounds(300, 150, 200, 50);
+		btnPlay.setBounds(300, 150, 185, 50);
 		btnPlay.setIcon(new ImageIcon("src/sw/resource/image/PlayButton.png"));
 		
 		JButton btnScoreboard = new JButton("Scoreboard");
 		add(btnScoreboard);
-		btnScoreboard.setBounds(300, 225, 200, 50);
+		btnScoreboard.setBounds(300, 225, 190, 50);
 		btnScoreboard.setIcon(new ImageIcon("src/sw/resource/image/ScoreboardButton.png"));
 		
 		JButton btnCredits = new JButton("Credits");
 		add(btnCredits);
-		btnCredits.setBounds(300, 300, 200, 50);
+		btnCredits.setBounds(300, 300, 190, 50);
 		btnCredits.setIcon(new ImageIcon("src/sw/resource/image/CreditsButton.png"));
 		
 		
