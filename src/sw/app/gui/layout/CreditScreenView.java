a7fbdb64b83024b973c0f70c7ec604caef5c1327
tnhuynh <tnhuynh@8e881b8d-9e2d-430c-b0ad-9d7c9e80049a>
2015-04-13 22:45:20 +0000

    Adjusted credit panel size.

--- src/sw/app/gui/layout/CreditScreenView.java
+++ src/sw/app/gui/layout/CreditScreenView.java
@@ -1,5 +1,6 @@
 package sw.app.gui.layout;
 
+import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.GroupLayout;
@@ -7,7 +8,6 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.SwingConstants;
 
 public class CreditScreenView extends JPanel implements IView{
@@ -23,6 +23,8 @@ public class CreditScreenView extends JPanel implements IView{
 	}
 	
 	public void initialize() {
+		
+		setPreferredSize(new Dimension(800,550));
 		
 		JButton btnBack = new JButton("Back");
 		btnBack.addActionListener(new MainMenuController(lm));
@@ -43,7 +45,7 @@ public class CreditScreenView extends JPanel implements IView{
 		label_2.setFont(new Font("French Script MT", Font.PLAIN, 25));
 		
 		JLabel label_3 = new JLabel("Steven Yevchak");
-		label_3.setHorizontalAlignment(SwingConstants.RIGHT);
+		label_3.setHorizontalAlignment(SwingConstants.CENTER);
 		label_3.setFont(new Font("French Script MT", Font.PLAIN, 25));
 		
 		JLabel label_4 = new JLabel("Dr. George Heineman");
@@ -51,47 +53,50 @@ public class CreditScreenView extends JPanel implements IView{
 		label_4.setFont(new Font("French Script MT", Font.PLAIN, 25));
 		
 		JLabel lblNewLabel_1 = new JLabel("Special Thanks to");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setFont(new Font("Forte", Font.PLAIN, 35));
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(191)
-					.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addGap(363))
+					.addGap(319)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 162, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(319, Short.MAX_VALUE))
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(155)
+					.addGap(289)
 					.addComponent(label, GroupLayout.PREFERRED_SIZE, 223, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(326, Short.MAX_VALUE))
+					.addContainerGap(288, Short.MAX_VALUE))
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(184)
+					.addGap(318)
 					.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 164, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(356, Short.MAX_VALUE))
+					.addContainerGap(318, Short.MAX_VALUE))
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(135)
+					.addGap(321)
+					.addComponent(label_2, GroupLayout.PREFERRED_SIZE, 159, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(320, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(331)
+					.addComponent(label_3, GroupLayout.PREFERRED_SIZE, 139, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(330, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(269)
 					.addComponent(lblNewLabel_1)
-					.addContainerGap(306, Short.MAX_VALUE))
+					.addContainerGap(268, Short.MAX_VALUE))
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(204)
+					.addGap(317)
 					.addComponent(label_4)
-					.addContainerGap(334, Short.MAX_VALUE))
-				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-					.addGap(18)
-					.addComponent(label_2, GroupLayout.DEFAULT_SIZE, 266, Short.MAX_VALUE)
-					.addGap(100))
+					.addContainerGap(317, Short.MAX_VALUE))
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(72)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(label_3, GroupLayout.PREFERRED_SIZE, 389, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnBack))
-					.addGap(243))
+					.addContainerGap(735, Short.MAX_VALUE)
+					.addComponent(btnBack)
+					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(50)
 					.addComponent(lblNewLabel)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGap(11)
 					.addComponent(label)
 					.addGap(11)
 					.addComponent(label_1)
@@ -103,13 +108,13 @@ public class CreditScreenView extends JPanel implements IView{
 					.addComponent(lblNewLabel_1)
 					.addGap(11)
 					.addComponent(label_4)
-					.addGap(18)
+					.addGap(177)
 					.addComponent(btnBack)
-					.addGap(16))
+					.addContainerGap())
 		);
 		setLayout(groupLayout);
 	}
-
+	
 	@Override
 	public void cleanup() {
 		// TODO Auto-generated method stub
