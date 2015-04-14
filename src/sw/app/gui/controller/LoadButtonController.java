4f3142e8ad9de59704cb4f361b2385a590f51fe0
tnhuynh <tnhuynh@8e881b8d-9e2d-430c-b0ad-9d7c9e80049a>
2015-04-14 21:26:37 +0000

    Added controllers for the load, save, and randomize buttons

---
+++ src/sw/app/gui/controller/LoadButtonController.java
@@ -0,0 +1,22 @@
+package sw.app.gui.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import sw.app.gui.view.LayoutManager;
+
+public class LoadButtonController implements ActionListener {
+
+	LayoutManager lm;
+	
+	public LoadButtonController(LayoutManager lm) {
+		this.lm = lm;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
