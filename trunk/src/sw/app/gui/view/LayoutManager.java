fe5d13c3f619a5f7ba87b3af4043766f8de9ce75
vchowdhary <vchowdhary@8e881b8d-9e2d-430c-b0ad-9d7c9e80049a>
2015-04-20 18:05:29 +0000

    Remove debug statements

--- trunk/src/sw/app/gui/view/LayoutManager.java
+++ trunk/src/sw/app/gui/view/LayoutManager.java
@@ -38,38 +38,30 @@ public class LayoutManager {
 	}
 	
 	public void switchToMainMenu(){
-		System.out.println("Going to Main Menu");
 		current = mmv;
 		f.switchToScreen(mmv);
 	}
 	
 	public void switchToGameplayView(){
-		System.out.println("Going to gameplay view");
 		current = gv;
 		f.switchToScreen(gv);
 	}
 	
 	public void switchToScoreBoardView(){
-		System.out.println("Going to score board view");
 		current = sbv;
 		f.switchToScreen(sbv);
 	}
 	
 	public void switchToCreditScreenView(){
-		System.out.println("Going to credit view");
-		//TODO Need to make credit view object
 		f.switchToScreen(csv);
 	}
 	
 	public void switchToPlayView(){
-		System.out.println("Going to play view");
 		current = pgs;
 		f.switchToScreen(pgs);
-		//TODO Need to make play view object
 	}
 	
 	public void switchToPostGameView() {
-		System.out.println("Going to postgame view");
 		current = pgv;
 		f.switchToScreen(pgv);
 	}
