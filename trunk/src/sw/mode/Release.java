7a235709c64c86fcdd542deb401be2a777425ad5
vchowdhary <vchowdhary@8e881b8d-9e2d-430c-b0ad-9d7c9e80049a>
2015-04-24 16:52:43 +0000

    Fix bug with release mode.

--- trunk/src/sw/mode/Release.java
+++ trunk/src/sw/mode/Release.java
@@ -20,16 +20,26 @@ import sw.common.system.manager.IResourceManager;
  */
 public class Release implements IMode {
 	/** Holds the location on the board which must be filled with 6s. */
-	boolean[][] sixLocation;	
+	boolean[][] sixLocation = new boolean[9][9];	
 
+	/**
+	 * TODO Remove this Constructor after testing phase is done...
+	 */
 	public Release(){
 		
 	}
 	
+	/**
+	 * Constructor for the release mode. 
+	 * @param sixLocation a 9x9 boolean array which holds true for the locations that must be filled with 6s.
+	 */
 	public Release(boolean[][] sixLocation){
 		this.sixLocation = sixLocation;		
 	}	
 	
+	/* (non-Javadoc)
+	 * @see sw.common.model.controller.IMode#getResourceManger()
+	 */
 	@Override
 	public IResourceManager getResourceManger() {
 		return new CommonResourceManager();
@@ -43,11 +53,17 @@ public class Release implements IMode {
 		return "Release";
 	}
 
+	/* (non-Javadoc)
+	 * @see sw.common.model.controller.IMode#getBoardController()
+	 */
 	@Override
 	public BoardController getBoardController() {
 		return new MoveSelection();
 	}
 
+	/* (non-Javadoc)
+	 * @see sw.common.model.controller.IMode#isValid(sw.common.model.controller.IMove)
+	 */
 	@Override
 	public boolean isValid(IMove m) {
 		System.out.println("Calling on release valid");
@@ -57,6 +73,9 @@ public class Release implements IMode {
 		return true;
 	}	
 	
+	/* (non-Javadoc)
+	 * @see sw.common.model.controller.IMode#hasFinished(sw.common.model.entity.Game, sw.common.model.entity.Statistics)
+	 */
 	@Override
 	public boolean hasFinished(Game g, Statistics winStats) {
 		// TODO Auto-generated method stub
@@ -64,7 +83,7 @@ public class Release implements IMode {
 			return false;
 		
 		for (int i = 0; i<sixLocation.length; i++){
-			for (int j = 0; j<sixLocation[i].length; i++){
+			for (int j = 0; j<sixLocation[i].length; j++){
 				if (g.getBoard().getColumn(i).getTile(j).getValue() != 6 && sixLocation[i][j])
 					return false;
 			}
