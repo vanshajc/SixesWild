f8398d5c1df13f362fdd2313894ca63b525c87fe
Vanshaj Chowdhary <vchowdhary@wpi.edu>
2015-04-25 04:12:40 +0000

    Add some test class

---
+++ tests/sw/common/model/controller/TestMoveSelection.java
@@ -0,0 +1,19 @@
+package sw.common.model.controller;
+
+import junit.framework.TestCase;
+
+public class TestMoveSelection extends TestCase{
+	// testing this is kind of hard.. i dont really want to do this.. 
+	protected void setUp(){
+		
+	}
+	
+	protected void tearDown(){
+		
+	}
+	
+	public void testMoves(){
+		
+	}
+
+}
