e3d2a17fc6b47f60e1acdba285cc88ef5ca20369
scyevchak <scyevchak@8e881b8d-9e2d-430c-b0ad-9d7c9e80049a>
2015-04-28 21:06:26 +0000

    added six release sqaure to lvlbuilder

--- src/sw/builder/gui/controller/DisplayBoardController.java
+++ src/sw/builder/gui/controller/DisplayBoardController.java
@@ -33,56 +33,64 @@ public class DisplayBoardController implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		//bbp = new BuilderBoardPanel(blm);
-//		String[][] b = lbv.getBoard();
-		//setBoardView(b);
-		//blm.switchToBuilderBoard();
+		// bbp = new BuilderBoardPanel(blm);
+		// String[][] b = lbv.getBoard();
+		// setBoardView(b);
+		// blm.switchToBuilderBoard();
 		String[][] b = LevelBuilderView.getBoard();
 		Board b1 = setBoardView(b);
 		blm.switchToScreen(LevelBuilderView.boardPanel);
 		LevelBuilderView.boardPanel.getBoard().copy(b1);
 		System.out.println("here");
-		LevelBuilderView.boardPanel.addMouseListener(new MouseAdapter() {			
+		LevelBuilderView.boardPanel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				System.out.println("in click event");
 				blm.switchToLevelBuilder();
-			}			
-		});
-		//bbp.setBoardView(b);
-	}
-	public static Board setBoardView(String[][] b) {
-		Board b1 = new Board();
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				String val = b[i][j].substring(0, 1);
-				int value = Integer.parseInt(val);
-				String mul = b[i][j].substring(2);
-				int mult = Integer.parseInt(mul);
-				Point p1 = new Point(i, j);
-				Tile t1 = null;
-				if (value != 0 && mult != 0) {
-					t1 = new Tile (value, mult);
-				}
-				CreateButtonController.board.getSquare(p1).setTile(t1);
 			}
-		}		
-		b1 = CreateButtonController.board;
-		return b1;
-		//b.copy(b1);
-		
-		//boardPanel.setBoard(b1);
-		//boardPanel.initialize();
-		
-		
-//		for (int i = 0; i < 9; i++) {
-//			for (int j = 0; j < 9; j++) {
-//				Point p1 = new Point(i, j);
-//				System.out.print(b1.getSquare(p1).getTile().getValue() + " ");
-//			}
-//			System.out.println("");
-//		}
-		
+		});
+		// bbp.setBoardView(b);
 	}
 
+	public static Board setBoardView(String[][] b) {
+		Board b1 = new Board();
+		Tile t1;
+		int mult = -1;
+		int value = -1;
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				Point p1 = new Point(i, j);
+				String val = b[i][j].substring(0, 1);
+				String mul = b[i][j].substring(2);
+				if (!val.equals("*") && !mul.equals("*")) {
+					value = Integer.parseInt(val);
+					mult = Integer.parseInt(mul);
+				}
+				if (mul.equals("*") || val.equals("*")) {
+					CreateButtonController.board.getSquare(p1).setTile(null);
+					CreateButtonController.board.getSquare(p1).setOnlySix(true);
+				} else if (value == 0 || mult == 0) {
+					CreateButtonController.board.getSquare(p1).setTile(null);
+				} else if(value != -1 || mult != -1) {
+					t1 = new Tile(value, mult);
+					CreateButtonController.board.getSquare(p1).setTile(t1);
+				}
+			}
+		}
+		b1 = CreateButtonController.board;
+		return b1;
+		// b.copy(b1);
+
+		// boardPanel.setBoard(b1);
+		// boardPanel.initialize();
+
+		// for (int i = 0; i < 9; i++) {
+		// for (int j = 0; j < 9; j++) {
+		// Point p1 = new Point(i, j);
+		// System.out.print(b1.getSquare(p1).getTile().getValue() + " ");
+		// }
+		// System.out.println("");
+		// }
+
+	}
 }
